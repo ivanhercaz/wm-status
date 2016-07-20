14f8d97ed368d39e4050e03a86b8aa8ec0246f49
David Abián <da@davidabian.com>
2016-07-20 22:42:09 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -73,7 +73,7 @@ echo "<title>Wikimedia availability</title>"
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./status.css\">"
 echo "</head>"
 echo "<body>"
-echo "<h1>Availability of non-WMF Wikimedia webpages</h1>"
+echo "<h1>Availability of Wikimedia webpages not hosted by the WMF</h1>"
 
 chapters=(
     "wikimedia.at"
