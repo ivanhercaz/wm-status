246b9551bd95b68213d64e5ecf5445b232b07b42
David Abián <da@davidabian.com>
2016-07-20 22:22:09 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #
-# Copyright (C) 2016, David Abián <da [at] davidabian.com>
+#  Copyright (C) 2016, David Abián <da [at] davidabian.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Source: <https://github.com/davidabian/wm-status>
 
-seconds_timeout=6
+seconds_timeout=10
 
 function print_ok {
     echo "<td class=\"status status_ok\"><div class=\"status_scrolling\">$1</div></td>"
